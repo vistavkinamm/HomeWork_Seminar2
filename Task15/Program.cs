30c54f697ffa032d2356eb443f9131957800a11f
Marina Vystavkina <vistavkinamm@mail.ru>
2022-09-06 14:47:53 +0300

    Task_15

---
+++ Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число обозначающее день недели");
+int numberDay=Convert.ToInt32(Console.ReadLine());
+
+if (numberDay<1 || numberDay>7)
+{
+    Console.WriteLine("Ошибка");
+}
+if (numberDay==6 || numberDay==7)
+{
+    Console.WriteLine("Да - это выходной день!");
+}
+else if (numberDay>= 1 && numberDay<=5)
+{
+    Console.WriteLine("Нет - это не выдодной день.");
+}
